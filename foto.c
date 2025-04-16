b41c08ba60c5de5776f9f0dab232ab67d9d285e8
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2025-04-16 11:43:18 -0300

    Create foto.c

---
+++ foto.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  int height=256;
+  int weight=256;
+
+  print("P#)
+
+  for(int i=0; i<height; i++){
+    for (int j=0; j<weight; j++) {
+      print("%d \t %d \t %d \t /n", i, j, 0)
+    }
+  }
+return 9;
+}
